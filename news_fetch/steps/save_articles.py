22f0eccda072b6ea52014e5b30bfa6582145c7a4
suriyasureshok <240171.ad@rmkec.ac.in>
2025-06-15 13:18:49 +0530

    Workflow

--- news_fetch/steps/save_articles.py
+++ news_fetch/steps/save_articles.py
@@ -10,10 +10,12 @@ def save_db_step(df:pd.DataFrame) -> None:
     try:
         conn = psycopg2.connect('postgresql://neondb_owner:npg_XOkZ2jyGNd6a@ep-empty-flower-a8c046xv-pooler.eastus2.azure.neon.tech/neondb?sslmode=require')
         cursor = conn.cursor()
+        
         for index, row in df.iterrows():
             cursor.execute("""
                 INSERT INTO Storage_of_articles (title, description, date)
-                VALUES (%s, %s, %s, %s)
+                VALUES (%s, %s, %s)
+                RETURNING id
             """,
             (row["title"], row["description"], row["published_at"]))
             article_id = cursor.fetchone()[0]
@@ -28,13 +30,11 @@ def save_db_step(df:pd.DataFrame) -> None:
             if result:
                 category_id = result[0]
             else:
-                # If already exists, fetch id
                 cursor.execute("""
                     SELECT id FROM categories WHERE name = %s
                 """, (row["category"],))
                 category_id = cursor.fetchone()[0]
 
-            # Insert relation into article_categories
             cursor.execute("""
                 INSERT INTO article_categories (article_id, category_id)
                 VALUES (%s, %s)
@@ -43,8 +43,12 @@ def save_db_step(df:pd.DataFrame) -> None:
             
         logging.info('Data Stored successfully')
         conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        logging.error(f"Error in storing the data: {str(e)}")
+        raise CustomException(e, sys)
+    
+    finally:
         cursor.close()
         conn.close()
-    except Exception as e:
-        logging.error(f"Error in storing the data")
-        raise CustomException(e,sys)
